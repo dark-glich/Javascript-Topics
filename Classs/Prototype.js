bf537defe99a458f8cc4aee1501292d6f3c2a550
dark-glich <mohdbilal9808464636@gmail.com>
2021-04-30 07:45:44 +0530

    Edited Classes

--- Classs/Prototype.js
+++ Classs/Prototype.js
@@ -1,14 +1,27 @@
+/*
+Prototype :
+It is an object that is associated with every functions and objects by default in JavaScript
+All JavaScript objects inherit properties and methods from a prototype.
+*/
+
 // Old Constructor from Constructor.js
 function Books(name, author, date) {
     this.name = name;
     this.author = author;
     this.date = date;
-    //Removed function
 }
+
+//you can not add a new property to an existing object constructor:
+//add new properties to object constructors prototype property is used:
+
+//Prototype Object
+Books.prototype.language = 'English'
+
 //Prototype function
 Books.prototype.getSummary = function () {
     return `${this.name} - ${this.author} - ${this.date}`
 }
+
 //check if the books is revised
 Books.prototype.isChanged = function (year, revised) {
     this.date = year
@@ -18,7 +31,7 @@ Books.prototype.isChanged = function (year, revised) {
 //Prototype functions does not get displayed when instance is logged
 book1 = new Books('everything', 'everyone', '2015')
 
-// new value can also be using prototype method
+// Assigning a new date using prototype method
 console.log(book1); 
 book1.isChanged(2021, true)
 console.log(book1); 
